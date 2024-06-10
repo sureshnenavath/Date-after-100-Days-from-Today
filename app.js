c020a2e2e113f302c25bde71ac2d2b82789425fe
sureshnenavath <suryanss143@gmail.com>
2024-06-10 11:05:31 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const addDays = require('date-fns/addDays')
+const express = require('express')
+const app = express()
+let date1 = new Date()
+const date = addDays(
+  new Date(date1.getFullYear(), date1.getMonth(), date1.getDate()),
+  100,
+)
+app.get('/', (req, res) => {
+  res.send(`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`)
+})
+
+app.listen(3000)
+module.exports = app
