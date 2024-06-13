92fb19ebd8686ad5508bf45158b7c0b7571256c5
Nenavath Suresh <133812638+sureshnenavath@users.noreply.github.com>
2024-06-13 19:20:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const addDays = require('date-fns/addDays')
 const express = require('express')
 const app = express()
 let date1 = new Date()
-const date = addDays(
+/*const date = addDays(
   new Date(date1.getFullYear(), date1.getMonth(), date1.getDate()),
   100,
-)
+)*/
 app.get('/', (req, res) => {
   const date = addDays(
     new Date(date1.getFullYear(), date1.getMonth(), date1.getDate()),
