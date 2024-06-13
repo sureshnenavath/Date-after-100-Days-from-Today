60e26d60ebe09d4b1d9cc53f5717a261a3b77375
Nenavath Suresh <133812638+sureshnenavath@users.noreply.github.com>
2024-06-13 19:15:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const date = addDays(
   100,
 )
 app.get('/', (req, res) => {
+  const date = addDays(
+    new Date(date1.getFullYear(), date1.getMonth(), date1.getDate()),
+    100,
+  )
   res.send(`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`)
 })
 
